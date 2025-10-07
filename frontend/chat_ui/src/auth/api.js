2192f40ecc619a6bd20a166f5e1df53aeea76232
Utsav <utsav1072@gmail.com>
2025-10-07 22:40:43 +0530

    Set BackendEndpoint

--- frontend/chat_ui/src/auth/api.js
+++ frontend/chat_ui/src/auth/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ baseURL: 'https://rag-fx2h.onrender.com/api' })
 
 function getStoredTokens() {
   try {
